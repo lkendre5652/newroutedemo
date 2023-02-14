61aee7c1e2adc8094e888e7d522cec15aaa02271
Laxman kendre <106816249+lkendre5652@users.noreply.github.com>
2023-02-14 14:51:38 +0530

    s

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { Home } from './common/Home';
 import { Contact } from './common/Contact';
 import {Routes,Route, NavLink} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 function App() {
   return (<>
   <NavLink to="newroutedemo/home" >Home</NavLink>
   <NavLink to="newroutedemo/contact" >Contact</NavLink>
+  <HashRouter>
   <Routes>
     <Route exact path="newroutedemo/home" element={<Home />} />
     <Route  path="newroutedemo/contact" element={<Contact />} />
   </Routes>
+  </HashRouter>
   
   </>
   );
